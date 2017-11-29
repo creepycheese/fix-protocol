f471b0e2ea586460fb3cbd8a20e79f755b9a5965
Evgeniy Burdaev <hate2me@yandex.ru>
2017-11-29 16:37:12 +0300

    order fields

--- lib/fix/protocol/messages/new_order.rb
+++ lib/fix/protocol/messages/new_order.rb
@@ -16,12 +16,34 @@ module Fix
           sell: 2
         }
 
+        ORD_TYPES = {
+          market: 1,
+          limit: 2,
+          shop: 3
+        }
+
+        TIME_IN_FORCE_TYPES = {
+          good_till_cancel: 1,
+          immidiate_or_cancel:   2,
+          good_till_time: 'A',
+          good_during_time: 'B'
+        }
+
         unordered :body do
           field :account,             tag: 1
           field :cl_ord_id,           tag: 11, required: true
           field :handl_inst,          tag: 21, mapping: HANDL_INST_TYPES
           field :symbol,              tag: 55, required: true
           field :side,                tag: 54, required: true, mapping: SIDE
+          field :transact_time,       tag: 60, required: true, type: :timestamp, default: proc { Time.now }
+          field :order_qty,           tag: 38, required: true, type: :integer
+          field :ord_type,            tag: 40, required: true, mapping: ORD_TYPES
+          field :price,               tag: 40, required: true, type: :integer
+          field :time_in_force,       tag: 59, required: true, type: :integer, mapping: TIME_IN_FORCE_TYPES
+          field :expire_time,         tag: 126, type: :timestamp, default: proc { Time.now }
+          field :max_show,            tag: 210, type: :integer
+          field :ref_price,           tag: 11030, type: :integer
+          field :currency,            tag: 15, type: :integer
         end
       end
     end
